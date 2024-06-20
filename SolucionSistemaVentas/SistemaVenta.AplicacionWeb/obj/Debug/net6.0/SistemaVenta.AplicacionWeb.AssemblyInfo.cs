9ea57709a2a6f88b8333ae02c1f4ffba27b8039d
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-06-20 08:28:22 -0600

    Se agrego la vista de Perfil

--- SolucionSistemaVentas/SistemaVenta.AplicacionWeb/obj/Debug/net6.0/SistemaVenta.AplicacionWeb.AssemblyInfo.cs
+++ SolucionSistemaVentas/SistemaVenta.AplicacionWeb/obj/Debug/net6.0/SistemaVenta.AplicacionWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf29a32525877fe818bb28e9ca78ff5f078c610")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34704dc9569dce93225e2c5b20342f5266cf1010")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
